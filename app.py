df403d62fc9d5f1eaa5018294d1f3ad6deb95bb2
jayanta8509 <jayantameslova@gmail.com>
2025-12-16 20:47:47 +0530

    chore: Update Uvicorn server port from 8000 to 8067.

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8000,
+        port=8067,
         reload=True,
         log_level="info"
     )
